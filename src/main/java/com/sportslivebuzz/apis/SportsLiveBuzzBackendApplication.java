20e2064f8aac260ac2ae95eccd3a4d44921557b0
muntazim_ali_khan <muntazimalikhan@globussoft.in>
2024-02-29 22:15:23 +0530

    initial commit for sportslivebuzz

---
+++ src/main/java/com/sportslivebuzz/apis/SportsLiveBuzzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sportslivebuzz.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsLiveBuzzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsLiveBuzzBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportslivebuzz/apis/SportsLiveBuzzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportslivebuzz.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsLiveBuzzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
